2a6b813c290b41614521455448acdc4a353ff62d
pandajakob <jakobm.dev@gmail.com>
2024-11-21 10:32:20 +0100

    Dynamic array

--- src/main.c
+++ src/main.c
@@ -1,64 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
-typedef struct {
+typedef struct Job {
     int Id;
     char title[100];
-    double salary; //
-    char adress[100]; // navn på adresse
-    double timeFromAAU; // estimeret tid givet af de studerende
+    double salary; // kr/time
+    char adress[100]; 
+    double timeFromAAU; // min
     double distanceFromAAU; // km
 } Job;
 
-int read_csv(Job jobs[]);
+Job *readJobs(int *n);
 void printJobs(Job jobs[], int n);
 
 int main() {
+    int n = 0;
 
-    Job jobs[13];
+    Job *jobs = readJobs(&n);
 
-    read_csv(jobs);
-    printJobs(jobs, 13);
+    printJobs(jobs, n);
 
     return 0;
 }
 
-int read_csv(Job jobs[]) {
-
+Job *readJobs(int *n) {
     FILE *fp; //*fp is file pointer;
     char row[1000];
     char *token;
+
+    int capacity = 2; // initial capacity of 2 jobs.
     
-    fp = fopen("survey.txt", "r"); 
-    if (fp == NULL) {
-        printf("Error opening file");
-        return 1; 
+    Job *jobs = malloc(sizeof(Job)*capacity);
+    if (jobs == NULL) {
+        printf("Failed to allocate memory");
+        exit(EXIT_FAILURE);
     }
     
-    int read = 0, records = 0;
+    fp = fopen("./src/fakeSurveyData.txt", "r"); 
+
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    int read = 0, records = 0; // read holds the variables read in each record. Records is each new line
+
+    fgets(row, sizeof(row), fp); // makes sure we skip the first header line
 
     do {
+        //fscanf scans a row in survey.txt, into a given target. Our target is the jobs structure
         read = fscanf(fp, "%99[^,],%99[^,],%lf,%lf,%lf\n", 
                     jobs[records].title, jobs[records].adress,
-                    &jobs[records].salary, &jobs[records].timeFromAAU, &jobs[records].distanceFromAAU);
+                    &jobs[records].salary, &jobs[records].timeFromAAU, 
+                    &jobs[records].distanceFromAAU);
+        
+        jobs[records].Id = records;
 
-        if (read == 5) records++;
-
-        if (read != 5 && !feof(fp)) {
-            printf("File format incorrect\n");
-            return 1;
+        if (read == 5) { 
+            records++; 
+            (*n)++;
         }
+        
+        if (read != 5 && !feof(fp)) {
+            printf("Error: file format incorrect\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("n: %d\n", *n);
+        printf("capacity: %d\n", capacity);
+        // if we run out of capacity, it will give jobs 10 more space.
+        if (*n >= capacity) {
+            capacity *= 2;
+            jobs = realloc(jobs, sizeof(Job)*capacity);
+            if (jobs == NULL) {
+                printf("Failed to allocate memory");
+                exit(EXIT_FAILURE);
+            }
+        }
+
     } while (!feof(fp));
     
     if (ferror(fp)) {
-        printf("Error reading file \n");
-        return 1;
-        
+        printf("Error reading file\n");
+        exit(EXIT_FAILURE);
     }
-
     fclose(fp);
 
-    return 0;
+    return jobs;
 }
 
 void printJobs(Job jobs[],int n) {
@@ -66,5 +94,7 @@ void printJobs(Job jobs[],int n) {
     printf("\n array: %d", n);
 
     for (int i = 0; i < n; i++) 
-        printf("\n%s %s Løn: %.2lf tid fra aau: %.2lf, distance: %lf", jobs[i].title, jobs[i].adress, jobs[i].salary, jobs[i].timeFromAAU, jobs[i].distanceFromAAU);
+        printf("\n Id: %d, %s %s Løn: %.1lf tid fra aau: %.1lf, distance: %.1lf\n", jobs[i].Id, jobs[i].title, jobs[i].adress, jobs[i].salary, jobs[i].timeFromAAU, jobs[i].distanceFromAAU);
+
+
 }
